16d2d8e0929b35be7c2c53882d49275c6581478f
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-12-25 15:24:18 +0200

    done

--- properties-Types/ViewController.swift
+++ properties-Types/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // stored properity
+    var mohamedAge : Int = 25
+    
+    // computed properity
+    var baselAge : Int {
+        //Getter case as xcode do operations to get the value of baselAge
+        get {
+            return mohamedAge - 2
+        }
+        // settet case that baselAge will be in case of changing the value
+        set {
+            print("baselAge ===> \(newValue)")
+             
+        }
+    }
+    
+    // observer properity observe for the change of value , willset excute first the  didset come after
+    var myAge : Int = 20 {
+        willSet{
+           print("newvalue \(newValue)")
+        }
+        didSet{
+            print("oldvalue \(oldValue)")
+            if myAge >= 100{
+                print("Invalid Age")
+                myAge = 100
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        print(mohamedAge)
+        print(baselAge)
+        // if we don't have a setter value we can not set baselAge to = 20
+        baselAge = 20
+
+        myAge = 150
+        print(myAge)
     }
 
 
