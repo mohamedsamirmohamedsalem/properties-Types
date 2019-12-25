b4db3e50279ccd7f174ae5e2b42c1e2ebece78b5
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-12-25 14:13:29 +0200

    Initial Commit

---
+++ properties-Types/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  properties-Types
+//
+//  Created by Mohamed Samir on 12/25/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
